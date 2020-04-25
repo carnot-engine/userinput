2aea020f34fdde911e175ad7c0cc76a3a1d948e3
carnot-engine <ghost_shell@tutanota.com>
2020-04-26 03:13:21 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# userinput
+userinput is a simple rust library for checking the error during userinput<br>
+(https://github.com/carnot-engine/userinput/blob/master/uml/userinput.png)
+<br>
+eg:
+<br>
+<pre>
+fn main()
+{
+    println!("Enter the usize");
+    let input = userinput::usize();
+    println!("input:{} usize:{}",input,type_of(input));
+}
+
+output:
+Enter the usize
+25
+input:25 datatype:usize
+
+syntax : userinput::datatype() //usize,isize,f64,f32,i128,u128,i64,u64,i32,u32,i8,u8,char,string
+       : userinput::type_of(variable_name)
+
+sample:
+let input = userinput::usize();
+let input = userinput::string();
+let input = userinput::char();
+
+
+</pre>
+
---
+++ src/lib.rs
@@ -0,0 +1,191 @@
+
+pub fn string() -> String {
+    let mut input = String::new();
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    input
+}
+
+pub fn usize() -> usize {
+    let input = string();
+    match input.trim().parse::<usize>() {
+        Ok(input) => {
+            return input;
+        }
+        Err(er) => {
+            println!("Failed to parse to usize 😟\nError! :{}\ntry again", er);
+            usize()
+        }
+    }
+}
+
+pub fn u128() -> u128 {
+    let input = string();
+    match input.trim().parse::<u128>() {
+        Ok(input) => {
+            return input;
+        }
+        Err(er) => {
+            println!("Failed to parse to u128 😟\nError: {}\ntry again", er);
+            u128()
+        }
+    }
+}
+
+pub fn u64() -> u64 {
+    let input = string();
+    match input.trim().parse::<u64>() {
+        Ok(input) => {
+            return input;
+        }
+        Err(er) => {
+            println!("Failed to parse to u64 😟\nError: {}\ntry again", er);
+            u64()
+        }
+    }
+}
+
+pub fn u16() -> u16 {
+    let input = string();
+    match input.trim().parse::<u16>() {
+        Ok(input) => {
+            return input;
+        }
+        Err(er) => {
+            println!("Failed to parse to u16 😟\nError: {}\ntry again", er);
+            u16()
+        }
+    }
+}
+
+pub fn u8() -> u8 {
+    let input = string();
+    match input.trim().parse::<u8>() {
+        Ok(input) => {
+            return input;
+        }
+        Err(er) => {
+            println!("Failed to parse to u8 😟\nError: {}\ntry again", er);
+            u8()
+        }
+    }
+}
+
+pub fn isize() -> isize {
+    let input = string();
+    match input.trim().parse::<isize>() {
+        Ok(input) => {
+            return input;
+        }
+        Err(er) => {
+            println!("Failed to parse to isize 😟\nError: {}\ntry again", er);
+            isize()
+        }
+    }
+}
+pub fn i128() -> i128 {
+    let input = string();
+    match input.trim().parse::<i128>() {
+        Ok(input) => {
+            return input;
+        }
+        Err(er) => {
+            println!("Failed to parse to i128 😟\nError: {}\ntry again", er);
+            i128()
+        }
+    }
+}
+pub fn i64() -> i64 {
+    let input = string();
+    match input.trim().parse::<i64>() {
+        Ok(input) => {
+            return input;
+        }
+        Err(er) => {
+            println!("Failed to parse to i64 😟\nError: {}\ntry again", er);
+            i64()
+        }
+    }
+}
+
+pub fn i32() -> i32 {
+    let input = string();
+    match input.trim().parse::<i32>() {
+        Ok(input) => {
+            return input;
+        }
+        Err(er) => {
+            println!("Failed to parse to i32 😟\nError: {}\ntry again", er);
+            i32()
+        }
+    }
+}
+
+pub fn i16() -> i16 {
+    let input = string();
+    match input.trim().parse::<i16>() {
+        Ok(input) => {
+            return input;
+        }
+        Err(er) => {
+            println!("Failed to parse to i16 😟\nError: {}\ntry again", er);
+            i16()
+        }
+    }
+}
+
+pub fn i8() -> i8 {
+    let input = string();
+    match input.trim().parse::<i8>() {
+        Ok(input) => {
+            return input;
+        }
+        Err(er) => {
+            println!("Failed to parse to i8 😟\nError: {}\ntry again", er);
+            i8()
+        }
+    }
+}
+
+pub fn f64() -> f64 {
+    let input = string();
+    match input.trim().parse::<f64>() {
+        Ok(input) => {
+            return input;
+        }
+        Err(er) => {
+            println!("Failed to parse to f64 😟\nError: {}\ntry again", er);
+            f64()
+        }
+    }
+}
+
+pub fn f32() -> f32 {
+    let input = string();
+    match input.trim().parse::<f32>() {
+        Ok(input) => {
+            return input;
+        }
+        Err(er) => {
+            println!("Failed to parse to f32 😟\nError: {}\ntry again", er);
+            f32()
+        }
+    }
+}
+
+pub fn char() -> char {
+    let input = string();
+    match input.trim().parse::<char>() {
+        Ok(input) => {
+            return input;
+        }
+        Err(er) => {
+            println!("Failed to parse to char 😟\nError: {}\ntry again", er);
+            char()
+        }
+    }
+}
+pub fn type_of<T>(_: T) -> &'static str {
+    std::any::type_name::<T>()
+}
